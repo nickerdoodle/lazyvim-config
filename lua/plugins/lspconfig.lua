179457c1be80b0c0dcf3749536bbc2c303f62ebb
Nick Mahe <nicholasmahe@gmail.com>
2025-11-05 00:33:40 -0700

    update deprecated way of overriding keymap for lspconfig plugin

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,15 +1,6 @@
 -- LSP keymaps
 return {
   "neovim/nvim-lspconfig",
-  init = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    -- change a keymap
-    -- keys[#keys + 1] = { "K", "5k" }
-    -- disable a keymap (disable K -> vim.lsp.buf.hover())
-    keys[#keys + 1] = { "K", false }
-    -- add a keymap
-    keys[#keys + 1] = { "<leader>k", "<cmd>lua vim.lsp.buf.hover()<cr>" }
-  end,
   opts = {
     diagnostics = {
       virtual_text = {
@@ -19,6 +10,15 @@ return {
       underline = true, -- Keep underlines for all diagnostics
       update_in_insert = false, -- Don't show diagnostics while typing
       severity_sort = true,
+    },
+    servers = {
+      ["*"] = {
+        keys = {
+          -- disable K keymap that does vim.lsp.buf.hover()
+          { "K", false },
+          { "<leader>k", "<cmd>lua vim.lsp.buf.hover()<cr>", has = "hover" },
+        },
+      },
     },
   },
 }
